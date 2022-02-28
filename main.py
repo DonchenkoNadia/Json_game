3fcd25c87e77a7c17dfa464a7cdb75d7f6d574b7
Nadia <DonchenkoNadia@gmail.com>
2022-02-28 18:56:24 +0100

    creating directories

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import requests
+import os
 
-r = requests.get("https://jsonplaceholder.typicode.com/posts/1/comments")
+def printElem(el):
+    print("{")
+    for k in el.keys():
+        if type(el.get(k)) is dict:
+            print(f"   \"{k}\": ")
+            printElem(el.get(k))
 
-posts = r.json()
-res = []
+        else:
+            print(f"   \"{k}\": {el.get(k)}, ")
+    print("},")
+
+r1 = requests.get("https://jsonplaceholder.typicode.com/posts")
+r2 = requests.get("https://jsonplaceholder.typicode.com/photos")
+
+posts = r1.json()
+photos = r2.json()
 
 for post in posts:
-    res.append([post.get("email"), post.get("body")])
+    i = 0
+    while i < len(photos):
+        if photos[i].get("id") == post["id"]:
+            post["photos"] = photos[i]
+            break
+        i += 1
 
-for r in res:
-    print(r)
+for post in posts:
+    printElem(post)
+    dirName = str(post.get("id"))
+    try:
+        # Create target Directory
+        #os.mkdir(dirName)
+        print("Directory ", dirName, " Created ")
+    except FileExistsError:
+        print("Directory ", dirName, " already exists")
