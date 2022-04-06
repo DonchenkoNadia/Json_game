66ed4efe47a2a29185f2b21c3e5bc5eefb11b903
Nadia <DonchenkoNadia@gmail.com>
2022-04-06 18:32:56 +0200

    Using Pandas to read Exel file writing dict to JSON

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+import pandas as pd
+import json
+
+df = pd.read_excel('our_data.xlsx')
+jsonFile = open("data_sample1.json", "w")
+
+for i in range(1, 5):
+    temp = {}
+    temp["date"] = str(df.iloc[i][0])
+    temp["avg"] = df.iloc[i][1:].mean()
+    temp["min"] = df.iloc[i][1:].min()
+    temp["max"] = df.iloc[i][1:].max()
+    print(i)
+    print(temp)
+    jsonString = json.dumps(temp)
+    jsonFile.write(jsonString)
+jsonFile.close()
+
+#print(df.iloc[i][1:].mean())
+
+
+
+
+'''
 import requests
+
 from datetime import datetime
 
 url = 'https://reqres.in/api/users'
@@ -19,4 +44,4 @@ while len(ans) < 4:
     ans[-1]["createdAt"] = s.replace(microsecond=0)
 
 print(ans)
-
+'''
